7868406f95dd5f6b1714c5b69ba334386bea6cb1
Kaiizer <haniamiine@gmail.com>
2025-04-28 22:55:50 +0200

    adding new parameters

--- src/components/Debug/Debug.tsx
+++ src/components/Debug/Debug.tsx
@@ -13,12 +13,19 @@ const Debug = ({ enabled = false }: any) => {
   const code = searchParams.get('code') // "GBF"
   const isDebug = searchParams.get('debugMode') // "true"
 
+  const charId = searchParams.get('ig_character_id') // "true" //ig_character_id =   10827
+  const discordId = searchParams.get('ig_discord_id') // "true" //ig_discord_id   =   332603174605291530
+
+  // Location: https://simple-rp.github.io/rp-manager-frame/?code=GBF&ig_character_id=10827&ig_discord_id=332603174605291530
+  
   if (!isDebug && !enabled) return <></>
   return (
     <div className="debug">
       <h2>Debug Info</h2>
       {line('Location', window.location.href)}
       {line('Code', code)}
+      {line('Character', charId)}
+      {line('Discord', discordId)}
     </div>
   )
 }
